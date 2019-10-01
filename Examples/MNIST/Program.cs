4c53146658ec3feb93a2a2b6ba009697ce68fbd5
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2019-09-30 23:22:30 -0400

    Adding Keras layers

--- Examples/MNIST/Program.cs
+++ Examples/MNIST/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
+using NumSharp;
+using Tensorflow;
+using Tensorflow.Hub;
+using static Tensorflow.Binding;
 
 namespace MNIST
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NDArray x_train, y_train;
+            NDArray x_valid, y_valid;
+            NDArray x_test, y_test;
+
+            var mnist = await MnistModelLoader.LoadAsync(".resources/fashion_mnist", oneHot: true);
+            x_train = mnist.Train.Data;
+            y_train = mnist.Train.Labels;
+            x_valid = mnist.Validation.Data;
+            y_valid = mnist.Validation.Labels;
+            x_test = mnist.Test.Data;
+            y_test = mnist.Test.Labels;
+
+            var model = keras.Sequential();
+            model.add(keras.layers.Dense(28*28));
+            model.add(keras.layers.Dense(128, activation: "relu"));
+            model.add(keras.layers.Dense(10, activation: "softmax"));
         }
     }
 }
